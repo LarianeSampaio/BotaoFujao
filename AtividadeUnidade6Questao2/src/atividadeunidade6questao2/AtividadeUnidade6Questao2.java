8a179e6e0f5c46ad0c71390960c43dff03171205
Lariane(desktop) <>
2020-06-15 13:58:14 -0300

    AtividadeUnidade6Questao2

---
+++ AtividadeUnidade6Questao2/src/atividadeunidade6questao2/AtividadeUnidade6Questao2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atividadeunidade6questao2;
+
+/**
+ *
+ * @author User
+ */
+public class AtividadeUnidade6Questao2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TelaPrincipal telaprincipal = new TelaPrincipal();
+        telaprincipal.setVisible( true );
+    }
+    
+}
