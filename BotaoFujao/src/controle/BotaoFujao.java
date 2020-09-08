e2bb7ccf5d02332972067edddb94728ca65cd278
Lariane(desktop) <User@DESKTOP-OV200A9>
2020-09-08 08:50:56 -0300

    BotaoFujao

---
+++ BotaoFujao/src/controle/BotaoFujao.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package controle;
+
+import tela.telabotao;
+
+/**
+ *
+ * @Lariane Sampaio
+ */
+public class BotaoFujao {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        telabotao telabotao = new telabotao();
+        telabotao.setVisible(true);
+        
+    }
+
+}
